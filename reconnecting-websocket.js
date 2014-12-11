a1af998cb5584d2c7f3cd8587cc137e662821afe
Jake Wilson <jake.e.wilson@gmail.com>
2014-12-11 14:50:49 -0700

    Changed settings to an object, and added passable options on instantiation. This allows someone to define settings immediately upon instantiation instead of after.
    It works like so:
    
    ```
    socket = new ReconnectingWebSocket(url, null, {debug:true, reconnectInterval:10000});
    ```
    
    The settings object are merged with the passed options object. Non-existent settings will be ignored when merging. So for example, if you passed an option of "hello":"world", it would be ignored, since there is no setting called "hello".
    
    After the merge, the settings are added to the object (this.debug, this.reconnectInterval, etc.). However you can still change these settings after the fact, just like you can now:
    
    ```
    socket = new ReconnectingWebSocket(...);
    socket.debug = true;
    ```
    
    Thus, backwards compatibility is preserved with this pull request.
    
    Overall this change will allow some more flexibility in the future. This will allow an "automaticOpen" option to be passed, as previously discussed. Some other thigs to consider is this approach would open the door for a change that allows event handlers to be passed through these options as well. So for example, you could eventually do:
    
    ```
    socket = new ReconnectingWebSocket(url, null, {
        debug:true,
        reconnectInterval:5000,
        onconnecting: function(){
            // some code
        }
    });
    ```
    
    That approach would fix the previously discussed issue about the socket connecting before the `onconnecting` event handler can be defined. This pull request does not allow you to do this, but it opens the door for this approach.

--- reconnecting-websocket.js
+++ reconnecting-websocket.js
@@ -40,7 +40,7 @@
  *  onopen  // sometime later...
  *  onmessage
  *  onmessage
- *  etc... 
+ *  etc...
  *
  * It is API compatible with the standard WebSocket API, apart from the following members:
  *
@@ -61,19 +61,38 @@
     }
 })(this, function () {
 
-    function ReconnectingWebSocket(url, protocols) {
-        // These can be altered by calling code.
+    function ReconnectingWebSocket(url, protocols, options) {
 
-        /** Whether this instance should log debug messages. */
-        this.debug = false;
-        /** The number of milliseconds to delay before attempting to reconnect. */
-        this.reconnectInterval = 1000;
-        /** The rate of increase of the reconnect delay. Allows reconnect attempts to back off when problems persist. */
-        this.reconnectDecay = 1.5;
-        /** The number of attempted reconnects since starting, or the last successful connection. */
-        this.reconnectAttempts = 0;
-        /** The maximum time in milliseconds to wait for a connection to succeed before closing and retrying. */
-        this.timeoutInterval = 2000;
+        // Default settings
+        var settings = {
+
+            /** Whether this instance should log debug messages. */
+            debug: false,
+
+            /** The number of milliseconds to delay before attempting to reconnect. */
+            reconnectInterval: 1000,
+
+            /** The rate of increase of the reconnect delay. Allows reconnect attempts to back off when problems persist. */
+            reconnectDecay: 1.5,
+
+            /** The number of attempted reconnects since starting, or the last successful connection. */
+            reconnectAttempts: 0,
+
+            /** The maximum time in milliseconds to wait for a connection to succeed before closing and retrying. */
+            timeoutInterval: 2000
+        }
+
+        // Merge settings with passed options
+        for (var key in options) {
+            if (typeof settings[key] !== 'undefined' ) {
+                settings[key] = options[key];
+            }
+        }
+
+        // Set object values from settings
+        for (var key in settings) {
+            this[key] = settings[key];
+        }
 
         // These should be treated as read-only properties
 
@@ -124,7 +143,7 @@
             if (self.debug || ReconnectingWebSocket.debugAll) {
                 console.debug('ReconnectingWebSocket', 'attempt-connect', url);
             }
-            
+
             var localWs = ws;
             var timeout = setTimeout(function() {
                 if (self.debug || ReconnectingWebSocket.debugAll) {
@@ -134,7 +153,7 @@
                 localWs.close();
                 timedOut = false;
             }, self.timeoutInterval);
-            
+
             ws.onopen = function(event) {
                 clearTimeout(timeout);
                 if (self.debug || ReconnectingWebSocket.debugAll) {
@@ -148,7 +167,7 @@
                 reconnectAttempt = false;
                 eventTarget.dispatchEvent(e);
             };
-            
+
             ws.onclose = function(event) {
                 clearTimeout(timeout);
                 ws = null;
